ccbea1c2f0a34b3d3d3150bbed37ec71cf21bd6c
dinoooop <dinoooop@gmail.com>
2018-07-01 20:43:34 +0530

    minified

--- js/scripts.js
+++ js/scripts.js
@@ -93,7 +93,7 @@ if (location.hostname == 'localhost') {
 var feed = new Instafeed({
   get: 'user',
   accessToken: '6783545862.d28727f.f07014b4ce4140a4b0ebba777ed01d2c',
-
+  template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>',
   sortBy: 'most-recent',
   limit: 4,
   links: true,
